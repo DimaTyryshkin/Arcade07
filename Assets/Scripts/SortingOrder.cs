46e9c081c865385dfb54d9558a983d826e2da28c
Дмитрий Тырышкин <yabloko9392@gmail.com>
2024-09-05 21:52:07 +0700

    изучайю тайлы. Пытаюсь добавить новые спрайты

---
+++ Assets/Scripts/SortingOrder.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace Action7
+{
+	[RequireComponent(typeof(SpriteRenderer))]
+	public class SortingOrder : MonoBehaviour
+	{
+		[SerializeField] bool isDynamic;
+		[SerializeField] int offset;
+        
+		Transform thisTransform;
+		SpriteRenderer spriteRenderer;
+         
+		public static float MaxY { get; set; }
+
+		public void Start()
+		{
+			thisTransform = transform;
+			spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+			UpdateOrder();
+
+			if (!isDynamic && Application.isPlaying)
+				Destroy(this);
+		}
+
+		void LateUpdate()
+		{
+			UpdateOrder();
+		}
+
+		void UpdateOrder()
+		{
+			spriteRenderer.sortingOrder = offset + GetOrderFromY(thisTransform.position.y);
+		}
+
+		public static int GetOrderFromY(float yPosition)
+		{
+
+
+			if (yPosition > MaxY)
+				Debug.LogError(yPosition > MaxY);
+
+
+			return (int)(-(yPosition - MaxY));
+		}
+	}
+}
---
+++ Assets/Scripts/SortingOrderSystem.cs
@@ -0,0 +1,32 @@
+﻿using GamePackages.Core.Validation;
+using NaughtyAttributes;
+using UnityEngine;
+
+namespace Action7
+{
+	public class SortingOrderSystem : MonoBehaviour
+	{
+		[SerializeField, IsntNull] Transform sortingOrderRoot;
+
+		[Button()]
+		void Start()
+		{
+			SortingOrder.MaxY = 1000;
+			var allSortingOrders = sortingOrderRoot.GetComponentsInChildren<SortingOrder>();
+			if (allSortingOrders.Length == 0)
+				return;
+
+			SortingOrder.MaxY = allSortingOrders[0].transform.position.y;
+
+			foreach (var sortingOrder in allSortingOrders)
+			{
+				float y = sortingOrder.transform.position.y;
+				if (y > SortingOrder.MaxY)
+					SortingOrder.MaxY = y;
+			}
+
+			foreach (var sortingOrder in allSortingOrders)
+				sortingOrder.Start();
+		}
+	}
+}
